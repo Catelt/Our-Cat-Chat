1cb9285b9f3609c42db0bb0bc56cb6a8c497c06d
Hồ Văn Duy <54297602+Catelt@users.noreply.github.com>
2023-04-03 22:38:51 +0700

    simple ui chat

--- lib/src/feature/view/home_view.dart
+++ lib/src/feature/view/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:my_chat_gpt/src/constants/app_sizes.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,8 +7,75 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Homescreen"),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+          toolbarHeight: 60,
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.symmetric(
+                vertical: Sizes.p16, horizontal: Sizes.p20),
+            child: SafeArea(
+              child: Container(
+                height: 60,
+                child: Row(
+                  children: [
+                    Icon(Icons.home),
+                    Gaps.w8,
+                    Text('Chat GPT'),
+                    Spacer(),
+                    Icon(Icons.volume_up),
+                    Gaps.w8,
+                    Icon(Icons.language)
+                  ],
+                ),
+              ),
+            ),
+          )),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 30,
+                    itemBuilder: (context, index) => Text(
+                          "Hello",
+                          style: TextStyle(fontSize: 20),
+                        )),
+              ),
+            ),
+            Positioned(
+                bottom: 20,
+                left: 0,
+                right: 0,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: Sizes.p20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Material(
+                        borderRadius: BorderRadius.circular(Sizes.p12),
+                        elevation: 8,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Say something',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(Sizes.p12),
+                              borderSide: BorderSide.none,
+                            ),
+                            fillColor: Colors.white,
+                            filled: true,
+                            contentPadding: const EdgeInsets.all(Sizes.p12),
+                          ),
+                        ),
+                      ))
+                    ],
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
